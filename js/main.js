b50430febd7341fc845acd3fb92c97e3d2ccf3d8
lemonpeel2019 <gilles.nigaud@gmail.com>
2021-02-26 09:19:40 +0100

    prompt command

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-alert("test");
+
+
+var person = prompt("Please enter your name", "");
+  if (person != null) {
+      console.log("Hello" + " " +person + " Bienvenue"); }
+
+      var userchoice = prompt("choose rock, paper, scissors");
+// lance un prompt sur ecran pour demander le choix
