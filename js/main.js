0e0e188b50b0535426b5ce953094654d29fde216
lemonpeel2019 <gilles.nigaud@gmail.com>
2021-02-26 09:55:54 +0100

    computer choice

--- js/main.js
+++ js/main.js
@@ -3,6 +3,20 @@
 var person = prompt("Please enter your name", "");
   if (person != null) {
       console.log("Hello" + " " +person + " Bienvenue"); }
+      // enter person name
 
       var userchoice = prompt("choose rock, paper, scissors");
-// lance un prompt sur ecran pour demander le choix
+// ask for choice
+
+// making computer choice
+var computerchoice = Math.random();
+if (computerchoice <0.34) {
+    computerchoice = "rock";
+}
+    else if (computerchoice <=0.67) {
+        computerchoice = "paper";
+    }
+    else {
+        computerchoice = "scissors";
+    }
+    console.log(computerchoice);
